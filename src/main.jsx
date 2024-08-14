678de4203c5b21f07458b3000947d2383a1bfc1e
Dip <dinnjain02@gmail.com>
2024-08-15 00:36:30 +0530

    blogs functionality with react-redux and redux-toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import {Provider} from 'react-redux'
+import {store} from './app/store.js'
+import Blogs from './components/Blogs/Blogs.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<App/>} />
+      <Route path='/blogs' element={<Blogs/>} />
+    </>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
